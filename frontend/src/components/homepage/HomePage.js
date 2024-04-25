5949ed13c4b4ee1bac8f9a9d2f075e078eb8642a
Ani Chilingaryan <chilingaryan12@gmail.com>
2024-04-25 14:02:41 -0700

    Adding a photo to homepage

--- frontend/src/components/homepage/HomePage.js
+++ frontend/src/components/homepage/HomePage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { homepageText } from "../../texts";
+import image from "../../stocktrends_homepage.png";
+
 /** Homepage of site.
  *
  * Routes -> Homepage
@@ -11,12 +13,14 @@ function Homepage() {
     return (
         <div className="Homepage">
             <div className="container text-center">
-                <h1 className="mb-4 font-weight-bold">Welcome to Stocktrends</h1>
+                <h1 className="mb-4 font-weight-bold" style={{ color: '#007bff' }}>Welcome to Stocktrends</h1>
                 <p className="lh-base">{homepageText}</p>
                 <Link className="btn btn-primary font-weight-bold mr-3"
                     to="/strategies">
                     Strategies
                 </Link>
+                <br></br>
+                <img src={image} alt="Stocktrends Dashboard" style={{ maxWidth: '100%', height: 'auto' }} />
             </div>
         </div>
     );
